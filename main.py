36c24d2ae7f40f68cb8278e3adccf5646e0edf56
Arzaq <masarzaq48@gmail.com>
2024-06-25 08:17:15 +0700

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
@@ -41,3 +42,6 @@ print(f"Confusion Matrix:\n{conf_matrix}")
 
 class_report = classification_report(y_test, y_pred)
 print(f"Classification Report:\n{class_report}")
+
+plt.scatter(range(len(y_pred)), y_pred)
+plt.show()
